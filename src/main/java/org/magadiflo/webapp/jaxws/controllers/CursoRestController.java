0f841a3d876cfe6f23ee8d141e1566f9c9ba42c4
Martín <magadiflo@gmail.com>
2022-01-28 20:02:23 -0500

    JAX-RS - Trabajando con XML

--- src/main/java/org/magadiflo/webapp/jaxws/controllers/CursoRestController.java
+++ src/main/java/org/magadiflo/webapp/jaxws/controllers/CursoRestController.java
@@ -2,10 +2,7 @@ package org.magadiflo.webapp.jaxws.controllers;
 
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.magadiflo.webapp.jaxws.models.Curso;
@@ -16,7 +13,7 @@ import java.util.Optional;
 
 @RequestScoped
 @Path("/cursos")
-@Produces(MediaType.APPLICATION_XML)
+@Produces(MediaType.APPLICATION_XML) //El formato que devuelve
 public class CursoRestController {
 
     @Inject
@@ -46,7 +43,53 @@ public class CursoRestController {
         return Response.status(Response.Status.NOT_FOUND).build();
     }
 
+    @POST
+    @Consumes(MediaType.APPLICATION_XML) //El formato que recibe
+    public Response crear(Curso curso) {
+        try {
+            Curso nuevoCurso = this.service.guardar(curso);
+            return Response.ok(nuevoCurso).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.serverError().build();
+        }
+    }
 
+    @PUT
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response editar(@PathParam("id") Long id, Curso curso) {
+        Optional<Curso> cursoOptional = this.service.porId(id);
+        if (cursoOptional.isPresent()) {
+            Curso nuevoCurso = cursoOptional.get();
+            nuevoCurso.setNombre(curso.getNombre());
+            nuevoCurso.setDescripcion(curso.getDescripcion());
+            nuevoCurso.setDuracion(curso.getDuracion());
+            nuevoCurso.setInstructor(curso.getInstructor());
+            try {
+                this.service.guardar(nuevoCurso);
+                return Response.ok(nuevoCurso).build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Response.serverError().build();
+            }
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 
-
+    @DELETE
+    @Path("/{id}")
+    public Response eliminar(@PathParam("id") Long id) {
+        Optional<Curso> cursoOptional = this.service.porId(id);
+        if (cursoOptional.isPresent()) {
+            try {
+                this.service.eliminar(id);
+                return Response.noContent().build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return Response.serverError().build();
+            }
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 }
